8750557fead8a0f49b9fc376ce9f638fd2cacce8
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-10-22 16:34:44 -0400

    added styling and fontawesome

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# qr-code-me
-what it says!
+# QR Code Me!
+
+## Description
+
+## Installation
+
+N/A
+
+## Usage
+
+Type in the link of the website you would like to generate a QR code for, click "Generate", and then click "Download" if you wish to download a .png version of the QR code.
--- src/components/QrCodeBox.jsx
+++ src/components/QrCodeBox.jsx
@@ -1,15 +1,21 @@
 import QRCode from "qrcode";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 function QrCodeBox() {
   const [link, setLink] = useState("");
   const [qr, setQr] = useState("");
+  const [placeholder, setPlaceholder] = useState("e.g. https://yourlink.com");
+  const [isRed, setIsRed] = useState(false);
 
   const GenerateQRCode = () => {
+    setQr("");
+
     QRCode.toDataURL(
       link,
       {
-        width: 500,
+        width: 200,
         margin: 2,
         color: {
           dark: "#000000",
@@ -17,9 +23,10 @@ function QrCodeBox() {
         },
       },
       (err, link) => {
-        if (err) return console.error(err);
-
-        console.log(link);
+        if (err) {
+          setIsRed(true);
+          setPlaceholder("Link is required.");
+        }
         setQr(link);
       }
     );
@@ -28,19 +35,30 @@ function QrCodeBox() {
   return (
     <div className="qrcodebox">
       <h1> QR Code Me! </h1>
-      <input
-        type="text"
-        placeholder="e.g. https://yourlink.com"
-        value={link}
-        onChange={(e) => setLink(e.target.value)}
-      />
-      <button onClick={GenerateQRCode}> Generate! </button>
+      <div className="enterLink">
+        <div>
+          <h2>STEP 1:</h2>
+          <p> Enter Link:</p>
+        </div>
+
+        <input
+          type="text"
+          placeholder={placeholder}
+          className={`input ${isRed ? "red-placeholder" : ""}`}
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+        />
+      </div>
+      <div className="hitGenerate">
+        <h2>STEP 2:</h2>
+        <button onClick={GenerateQRCode}> Click here to get your code! </button>
+      </div>
       {qr && (
         <>
           <img src={qr} />
-          <button href={qr} download="qrcode.png">
-            Download
-          </button>
+          <a className="downloadBtn" href={qr} download="qrcode.png">
+            Download <FontAwesomeIcon icon={faDownload} />
+          </a>
         </>
       )}
     </div>
